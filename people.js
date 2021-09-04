cb21a619867e0a9403a6460ffea0d047c882171e
lakindu <64328828+lakindu-banneheka@users.noreply.github.com>
2021-09-04 11:10:00 +0530

    new lesson

--- people.js
+++
@@ -1,8 +0,0 @@
-const people = ['lakindu', 'dilshan' , 'dehemi' , 'malith'];
-const ages = [12,10,60,59];
-
-// console.log(people);
-
-module.exports = {
-    ages,people // peopple:people , also we can exprot like this ==>  men : people 
-};
